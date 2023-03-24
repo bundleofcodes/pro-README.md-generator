d7498848298554018d97ff21a6142084e329cd5f
Raiya Young <121911949+bundleofcodes@users.noreply.github.com>
2023-03-24 17:33:56 -0400

    made edits to generateMardown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,42 +1,64 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+function renderLicenseBadge(License) {
+  if (License !== 'None') {
+    return `![Github license](https://img.shields.io/badge/license-$(license)-blue.svg)`;
+  }
+  return '';
+}
 
-// function renderLicenseBadge(license) {}
+function renderLicenseLink(License) {
+  if (License !== 'None') {
+    return `\n" [License](#license)\n`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+function renderLicenseSection(License) {
+  if (License !== 'None') {
+    return `## License
+    
+    This project is licensed under the $(License) license.`;
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  ## License:
-  [![License](https://img.shields.io/badge/license-${data.license}-blue.svg)]
-  ## Table of Contents
-  - [Description](#description)
+    ${renderLicenseBadge(data.license)}
+
+    ## Description
+
+    ${data.description}
+ 
+    ## Table of Contents
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Contributions](#contribution)
-  - [Testing](#testing)
-  - [Additional Info](#additional-info)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
 
-  ## Description:
-  ${data.description}
   ## Installation:
+
   ${data.installation}
+
   ## Usage:
+
   ${data.usage}
+
   ## License:
+
   ${data.license}
-  ## Contribution:
-  ${data.contribution}
-  ## Testing:
-  ${data.test}
-  ## Contact Information:
+
+  ## Contributing:
+
+  ${data.contributing}
+
+  ## Tests:
+  
+  ${data.tests}
+
+  ## Questions:
+
   - Github: [${data.github}](https://github.com/${data.github})
   - Email: [${data.email}](mailto:user@example.com)`;
 }
